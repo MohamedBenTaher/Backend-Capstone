e3a450980677ca7f1d3452373184a846023191b6
Mohamed Ben Taher <mbentaher1998@gmail.com>
2023-03-02 00:58:15 +0100

    models->booking+Menu

--- restaurant/models.py
+++ restaurant/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Booking(models.Model):
+    name=models.CharField(max_length=255)
+    guest_number=models.SmallIntegerField()
+    booking_date=models.DateTimeField()
+    def __str__(self):
+        return self.name
+    
+class Menu(models.Model):
+    title=models.CharField(max_length=255)
+    price=models.DecimalField(decimal_places=2,max_digits=6)
+    inventory=models.SmallIntegerField()
+    def __str__(self):
+        return self.title
